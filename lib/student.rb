dea682a3317b6c55657ad7284dadad6d8fdc5a1c
James Lenahan <james.k.lenahan@gmail.com>
2020-01-17 17:10:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,10 +87,8 @@ class Student
       SELECT * FROM students
       WHERE grade = ?
       SQL
-
-      DB[:conn].execute(sql, grade).map do |row|
-        self.new_from_db(row)
-      end
+    end
+    
   end
 
   def save
